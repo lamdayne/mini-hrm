949fc755db9eeaa862f9a55820e08cdf027a1726
lamdayne <nlam32428@gmail.com>
2026-05-16 20:26:51 +0700

    feat(exception): check nullpointerexception

--- src/main/java/com/lamdayne/minihrm/exception/GlobalExceptionHandler.java
+++ src/main/java/com/lamdayne/minihrm/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.lamdayne.minihrm.exception;
 import com.lamdayne.minihrm.dto.response.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -24,21 +25,30 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiResponse<?>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        String enumKey = e.getFieldError().getDefaultMessage();
+        FieldError fieldError = e.getFieldError();
+        if (fieldError == null) {
+            return ResponseEntity.badRequest().body(
+                    ApiResponse.builder()
+                            .message("Validation failed!")
+                            .build()
+            );
+        }
+
+        String enumKey = fieldError.getDefaultMessage();
         ErrorCode errorCode = ErrorCode.INVALID_ERROR_CODE;
 
         try {
             errorCode = ErrorCode.valueOf(enumKey);
         } catch (IllegalArgumentException ex) {
-            String fieldError = e.getFieldError().getField();
+            String fieldEr = e.getFieldError().getField();
             String classError = e.getFieldError().getObjectName();
-            log.error("Invalid ErrorCode with key = \"{}\", field = \"{}\", classError = \"{}\"", enumKey, fieldError, classError);
+            log.error("Invalid ErrorCode with key = \"{}\", field = \"{}\", classError = \"{}\"", enumKey, fieldEr, classError);
         }
 
         return ResponseEntity
                 .status(errorCode.getHttpStatus())
                 .body(ApiResponse.builder()
-                        .status(errorCode.getStatus())
+                        .status(errorCode.getCode())
                         .message(errorCode.getMessage())
                         .build()
                 );
